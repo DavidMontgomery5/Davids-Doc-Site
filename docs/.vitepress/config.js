cc29c86124fdea3b4ece2e928871ed4be0416f77
David Montgomery <davidjmontgomery350@gmail.com>
2022-05-25 14:36:03 -0500

    Final Mock VitePress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Vitepress Tutorial' , 
+    themeConfig: {
+        nav: [
+            {text: 'Home' , link: '/'}
+            {text: 'About' , link: '/about/'},
+            {text: 'Contact' , link: '/Contact/'},
+        ],
+    }
+}
